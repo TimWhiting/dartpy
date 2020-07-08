22d642102afff884261863bd70b9ece904aae321
Tim Whiting <tim@whitings.org>
2020-07-07 19:02:33 -0600

    refactor to monorepo format

---
+++ gen_dartpy/example/gen_dartpy_example.dart
@@ -0,0 +1,1 @@
+import 'package:gen_dartpy/gen_dartpy.dart';
---
+++ gen_dartpy/lib/gen_dartpy.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gen_dartpy;
+
+export 'src/gen_dartpy_base.dart';
