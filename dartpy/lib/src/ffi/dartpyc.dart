6a339badca3845eb5546569cdf199239443ec9e1
Tim Whiting <tim@whitings.org>
2021-01-10 15:40:09 -0700

    updates

--- dartpy/lib/src/ffi/dartpyc.dart
+++ dartpy/lib/src/ffi/dartpyc.dart
@@ -1,5 +1,3 @@
-import 'package:ffi/ffi.dart';
-
 import 'gen.dart';
 
 import 'dart:ffi' as ffi;
@@ -14,15 +12,3 @@ final dartpyc = DartPyC(
               '/usr/local/Frameworks/Python.framework/Versions/3.8/lib/libpython3.8.dylib')
           : throw UnimplementedError('${io.Platform} not supported'),
 );
-void main(List<String> args) {
-  dartpyc.Py_Initialize();
-  final python = '''
-from time import time, ctime
-print("Today is", ctime(time()))
-''';
-  final pystring = Utf8.toUtf8(python);
-  dartpyc.PyRun_SimpleString(pystring.cast<ffi.Int8>());
-  free(pystring);
-
-  print(dartpyc.Py_FinalizeEx());
-}
